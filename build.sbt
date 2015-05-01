df0db35847af32eb2da76b8b15ff3995b255d3ba
alexander svendsen <alexander.svendsen@bekk.no>
2015-05-01 23:20:47 +0200

    add keyboard input

--- build.sbt
+++ build.sbt
@@ -6,3 +6,6 @@ scalaVersion := "2.11.6"
 
 seq(lwjglSettings: _*)
 
+libraryDependencies += "com.typesafe.akka" % "akka-actor_2.11" % "2.3.9"
+
+libraryDependencies += "com.typesafe.akka" % "akka-testkit_2.11" % "2.3.9"
--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -1,8 +1,9 @@
+import org.lwjgl.input.Keyboard
 import org.lwjgl.opengl.GL11._
 import org.lwjgl.opengl.{Display, DisplayMode}
 
 object HelloWorld extends App {
-	val displayMode = new DisplayMode(640, 480)
+	val displayMode = new DisplayMode(800, 600)
 	Display.setTitle("LWJGL Test")
 	Display.setDisplayMode(displayMode)
 	Display.create()
@@ -10,7 +11,18 @@ object HelloWorld extends App {
 	Display.destroy()
 	sys.exit(0)
 
+	def pollInput() {
+		while (Keyboard.next()) {
+			if (Keyboard.getEventKeyState) {
+				if (Keyboard.getEventKey == Keyboard.KEY_A) {
+					println("A key")
+				}
+			}
+		}
+	}
+
 	def fader(color: Float, d: Int = -1) {
+		pollInput()
 		val mult = if(color > 1f) 1 else if(color < 0f) -1 else d
 		val newColor = color - (mult * 0.05f)
 
